343a0bcf4f4feba19947d7aa6fb8a6d74c54d172
Tuan <sontuandav@gmail.com>
2020-12-09 19:42:07 +0700

    tao tank

---
+++ tank.js
@@ -0,0 +1,16 @@
+function Tank(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.run = function () {
+        this.x += this.vX
+        this.y += this.vY
+    }
+    this.draw = function (ctx) {
+        let img = new Image()
+        img.src = "car.png"
+        ctx.drawImage(img, this.x, this.y);
+    }
+}
+}
