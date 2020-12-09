effef264b5eda92df2ae46db04ad1c4262f5f0e7
Tuan <sontuandav@gmail.com>
2020-12-09 20:09:22 +0700

    vi du

--- game.js
+++ game.js
@@ -2,8 +2,11 @@ function Game(canvas) {
     this.canvas = canvas;
     let tanks = [];
     let ctx = this.canvas.getContext("2d");
+    let tank = initNewTank();
+    console.log (tank)
+    tanks.push(tank)
     this.requestNextFrame = function () {
-        tanks.push(initNewTank());
+       
         moveTank();
     }
 
@@ -12,19 +15,24 @@ function Game(canvas) {
         drawTank();
     }
     function moveTank() {
-        for (let i = 1; i <= tanks.length; i++) {
+        for (let i = 0; i <= tanks.length; i++) {
+            console.log(tanks[i])
             tanks[i].run();
+            
         }
+        
     }
     function drawTank() {
-        for (let i = 1; i <= tanks.length; i++) {
+        for (let i = 0; i <= tanks.length; i++) {
             tanks[i].draw(ctx);
         }
     }
     function clearCanvas() {
         ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
     }
-    function initNewTank() {    
-        new Tank(1,1)
+    function initNewTank() { 
+        let tank = new Tank()   
+        console.log(tank)
+        return tank
     }
 }
