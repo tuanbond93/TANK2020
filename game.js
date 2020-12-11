2351d6db4cf88766d8685b01d6b8aaf105d2ae16
Tuan <sontuandav@gmail.com>
2020-12-11 20:41:45 +0700

    di chuyen tank

--- game.js
+++ game.js
@@ -11,25 +11,45 @@ function Game(canvas) {
         clearCanvas();
         drawTank();
     }
+
+    this.control = function() {
+        console.log(a)
+        if (a === 97) {
+            for (let i = 0; i < tanks.length; i++) {
+                tanks[i].runLeft();
+            }
+        }
+        if (a === 100) {
+            for (let i = 0; i < tanks.length; i++) {
+                tanks[i].runRight();
+            }
+        }
+        if (a === 119) {
+            for (let i = 0; i < tanks.length; i++) {
+                tanks[i].runUp();
+            }
+        }
+        if (a === 115) {
+            for (let i = 0; i < tanks.length; i++) {
+                tanks[i].runDown();
+            }
+        }
+    }
+    
     function initNewTank() {
         return new Tank(1,0)
     }
-    function keyDownHandler(evt) {
-        a = evt.keyCode;
-        console.log(a)
-    }
 
     function moveTank() {
-        for (let i = 0; i < tanks.length; i++) {
-            tanks[i].runDown();
-        }
+        //for (let i = 0; i < tanks.length; i++) {
+            //tanks[i].runRight();
+        //}
     }
     function clearCanvas() {
         ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
     }
     function drawTank() {
         for (let i = 0; i < tanks.length; i++) {
-            
             tanks[i].draw(ctx);
         }
     }
--- main.js
+++ main.js
@@ -2,11 +2,15 @@ let canvas = document.getElementById('tankCanvas');
 let game = new Game(canvas) 
 
 window.requestAnimationFrame(runGame)
+window.onkeypress = function(e){
+  a = e.keyCode
+  game.control(e.keyCode)
+}
 
 function runGame() {
  
   game.requestNextFrame()
   game.draw()
-
+  
   window.requestAnimationFrame(runGame)
 }
