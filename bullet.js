9f95e94c371b5f1f59565a3c1389103ab323758f
Tuan <sontuandav@gmail.com>
2020-12-14 19:50:20 +0700

    change const

--- bullet.js
+++ bullet.js
@@ -1,52 +1,53 @@
-function Tank(x, y) {
+function Bullet(x, y) {
     this.x = x;
     this.y = y;
     this.vx = 0;
-    this.vy = 1;
+    this.vy = 2;
     this.runLeft = function () {
-        this.vx = -1
+        this.vx = -2
         this.vy = 0
 
     }
     this.runRight = function () {
-        this.vx = 1
+        this.vx = 2
         this.vy = 0
 
     }
     this.runUp = function () {
         this.vx = 0
-        this.vy = -1
+        this.vy = -2
 
     }
     this.runDown = function () {
         this.vx = 0
-        this.vy = 1
+        this.vy = 2
 
     }
 
     this.run = function () {
+        
         this.x += this.vx
         this.y += this.vy
     }
     this.draw = function (ctx) {
-        if (this.vx === -1) {
+        if (this.vx === -2) {
             let img = new Image()
-            img.src = "tankLeft.png"
+            img.src = "bulletLeft.png"
             ctx.drawImage(img, this.x, this.y);
         }
-        if (this.vx === 1) {
+        if (this.vx === 2) {
             let img = new Image()
-            img.src = "tankRight.png"
+            img.src = "bulletRight.png"
             ctx.drawImage(img, this.x, this.y);
         }
-        if (this.vy === -1) {
+        if (this.vy === -2) {
             let img = new Image()
-            img.src = "tankUp.png"
+            img.src = "bulletUp.png"
             ctx.drawImage(img, this.x, this.y);
         }
-        if (this.vy === 1) {
+        if (this.vy === 2) {
             let img = new Image()
-            img.src = "tankDown.png"
+            img.src = "bulletDown.png"
             ctx.drawImage(img, this.x, this.y);
         }
     }
--- game.js
+++ game.js
@@ -1,49 +1,82 @@
 function Game(canvas) {
+    const CONTROL_MOVE_LEFT = 97
+    const CONTROL_MOVE_RIGHT = 100
+    const CONTROL_MOVE_UP = 119
+    const CONTROL_MOVE_DOWN = 115
+    const CONTROL_FIRE = 13
+
     this.canvas = canvas;
     let tanks = [];
+    let bullets = [];
     let ctx = this.canvas.getContext("2d");
     tanks.push(initNewTank());
+
     this.requestNextFrame = function () {
         moveTank();
+        moveBullet();
     }
     this.draw = function () {
-        
+
         clearCanvas();
         drawTank();
+        drawBullet();
     }
 
-    this.control = function() {
-        console.log(a)
-        if (a === 97) {
-            for (let i = 0; i < tanks.length; i++) {
-                tanks[i].runLeft();
-            }
-        }
-        if (a === 100) {
-            for (let i = 0; i < tanks.length; i++) {
-                tanks[i].runRight();
-            }
-        }
-        if (a === 119) {
-            for (let i = 0; i < tanks.length; i++) {
-                tanks[i].runUp();
-            }
-        }
-        if (a === 115) {
-            for (let i = 0; i < tanks.length; i++) {
-                tanks[i].runDown();
-            }
+    this.control = function (controlCode) {
+        console.log(controlCode)
+        switch (controlCode) {
+            case CONTROL_MOVE_LEFT:
+                for (let i = 0; i < tanks.length; i++) {
+                    tanks[i].runLeft();
+                }
+                break;
+
+            case CONTROL_MOVE_RIGHT:
+                for (let i = 0; i < tanks.length; i++) {
+                    tanks[i].runRight();
+                }
+                break;
+            case CONTROL_MOVE_UP:
+                for (let i = 0; i < tanks.length; i++) {
+                    tanks[i].runUp();
+                }
+                break;
+            case CONTROL_MOVE_DOWN:
+                for (let i = 0; i < tanks.length; i++) {
+                    tanks[i].runDown();
+                }
+                break;
+            case CONTROL_FIRE:
+                bullets.push(initNewBullet());
+                for (let i = 0; i < bullets.length; i++) {
+                    bullets[i].run();
+                    bullets[i].x = tanks[i].x
+                    bullets[i].y = tanks[i].y
+                    console.log(bullets[i])
+                    console.log(tanks[i])
+                    console.log(tanks[i].x)
+                }
+                break
         }
     }
-    
+
     function initNewTank() {
-        return new Tank(1,0)
+        return new Tank(0, 0)
+    }
+
+    function initNewBullet() {
+        return new Bullet()
     }
 
     function moveTank() {
-        //for (let i = 0; i < tanks.length; i++) {
-            //tanks[i].runRight();
-        //}
+        for (let i = 0; i < tanks.length; i++) {
+            tanks[i].run();
+        }
+    }
+    function moveBullet() {
+        for (let i = 0; i < bullets.length; i++) {
+            bullets[i].run();
+        }
     }
     function clearCanvas() {
         ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
@@ -53,5 +86,9 @@ function Game(canvas) {
             tanks[i].draw(ctx);
         }
     }
-    
+    function drawBullet() {
+        for (let i = 0; i < bullets.length; i++) {
+            bullets[i].draw(ctx)
+        }
+    }
 }
